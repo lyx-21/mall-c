6eaf8e6bd472e0a7fc1d59d450a6b5d0a00f1405
lyx-21 <2655659636@qq.com>
2021-09-19 20:33:50 +0800

    移动端配置

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <van-button plain type="info">朴素按钮</van-button>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+  width: 375px;
+  height: 20px;
+  background: #f40;
 }
 </style>
